a03cd930e814370a0e40a9b4633f15c66d839de5
moluh <morettilucash@gmail.com>
2022-05-26 22:56:23 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
   cors: {
-    origin: "http://localhost:8080",
+    origin: "http://localhost:8100",
     methods: ["GET", "POST"]
   }
 });
